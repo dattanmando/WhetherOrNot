2568cb8c9ec2d2d1eaa1a9c98c94e2cd38a66313
Tanner Chamberlain <tannerchamberlain8@gmail.com>
2025-03-10 20:22:17 -0600

    dist

---
+++ server/dist/service/historyService.js
@@ -0,0 +1,13 @@
+// TODO: Define a City class with name and id properties
+// class City {
+//   name: string;
+//   id: string;
+//   constructor(name: string, id: string) {
+//     this.name = name;
+//     this.id = id;
+//   }
+// }
+// TODO: Complete the HistoryService class
+class HistoryService {
+}
+export default new HistoryService();
